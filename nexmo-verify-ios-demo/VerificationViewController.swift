9d4a09a84aeba35edb2e809d1b1d5f91946e5f3c
ericgiannini <eric.giannini@yahoo.com>
2018-05-03 12:09:30 -0700

    uploading the boilerplate project

---
+++ nexmo-verify-ios-demo/VerificationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VerificationViewController.swift
+//  nexmo-verify-ios-demo
+//
+//  Created by Eric Giannini on 5/3/18.
+//  Copyright © 2018 Nexmo, Inc. All rights reserved.
+//
+
+import UIKit
+
+class VerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
